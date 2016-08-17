6f016a531562b39f1690aaccc3519a85698b97d2
freddy <freddy.marechal@gmail.com>
2016-08-17 13:38:02 +0100

    quik fix

--- data/machine_learning_projects.js
+++ data/machine_learning_projects.js
@@ -3,7 +3,7 @@ var machine_learning_projects =
         [ 	"Supervised Learning" ,
             "Predicting Boston Housing Prices" ,
             "Apply basic machine learning concepts on data collected for housing prices in the Boston, Massachusetts area to predict the selling price of a new home." ,
-            "projects/machine_learning/supervised_learning/predicting_boston_housing_prices/doc/Predicting%20Boston%20Housing%20Prices.pdf" ,
+            "/machine_learning/supervised_learning/predicting_boston_housing_prices/doc/Predicting%20Boston%20Housing%20Prices.pdf" ,
             "Python" ,
             "projects/machine_learning/supervised_learning/predicting_boston_housing_prices/code" ],
 
