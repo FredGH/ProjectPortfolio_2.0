1a2b1c95b1854e4b9fd733d466363e22ea86aa92
freddy <freddy.marechal@gmail.com>
2016-08-17 16:08:10 +0100

    more

---
+++ data/big_data_engineering_projects.js
@@ -0,0 +1,9 @@
+var big_data_engineering_projects =
+    [
+        [ 	"Programming" ,
+            "OOPS and Functional Programming in Scala" ,
+            "Introduction to Object Oriented  Programming in Scala" ,
+            "" ,
+            "Scala" ,
+            "/big_data_engineering/oops_and_functional_programming_in_scala" ]
+    ];
