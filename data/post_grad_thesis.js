7eebcbdf7d26c6f138dab80037888eff50277b3d
freddy <freddy.marechal@gmail.com>
2016-08-31 23:08:05 +0100

    small change

--- data/post_grad_thesis.js
+++ data/post_grad_thesis.js
@@ -3,7 +3,7 @@ var post_grad_thesis =
         [ 	"MSc in Software Development Thesis (Executive Summary)" ,
             "A Model Checking Tool to detect and resolve potential Redundancies in Entity Relationship models" ,
             "The aims are to prototype an enhanced version of a discover strategy proposed by Bowers (2002), and verify how efficient and correct this solution is in discovering potential redundant relationships" ,
-            "/ProjectPortfolio_2.0/tree/master/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf" ,
             "" ,
             "" ,
             "March-2008"],
