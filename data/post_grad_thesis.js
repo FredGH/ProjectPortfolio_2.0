2d3bc7e4dff4053daa2d3b96c3234a3ca86f523f
freddy <freddy.marechal@gmail.com>
2016-09-02 06:57:40 +0100

    small bug fixes

--- data/post_grad_thesis.js
+++ data/post_grad_thesis.js
@@ -1,17 +1,20 @@
 var post_grad_thesis =
     [
+
         [ 	"MSc in Software Development Thesis" ,
             "A Model Checking Tool to detect and resolve potential Redundancies in Entity Relationship models" ,
-            "The aims are to prototype an enhanced version of a discover strategy proposed by Bowers (2002), and verify how efficient and correct this solution is in discovering potential redundant relationships" ,
-            "Summary|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf|Full|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/dissertation.pdf" ,
+            "The aims are to prototype an enhanced version of a discovery strategy proposed by Bowers (2002) and verify how efficient and correct this solution is in discovering potential redundant relationships" ,
+            "Summary|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf" +
+            "|Full|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/dissertation.pdf" ,
             "Java" ,
             "/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/code" ,
             "Mar-2008",
             "EN"],
-        [ 	"Maitrise Banque, Finance et Assurance" ,
+        [ 	"Maitrise Banque Finance Assurance" ,
             "Credit Value-at-Risk (VaR)" ,
-            "Description and implementation  of the VaR applied to a credit portfolio, and review of the methodology limitations" ,
-            "/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_sommaire.pdf" ,
+            "Description and implementation  of the VaR applied to a credit portfolio and review of the methodology limitations" ,
+            "Table Of Content|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_sommaire.pdf" +
+            "|Introduction|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/credit_var_limites_introduction.pdf" ,
             "" ,
             "" ,
             "Jun-1998",
