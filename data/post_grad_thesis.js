96fc920bd07e598a0fb62a1be10d0618f21acff4
freddy <freddy.marechal@gmail.com>
2016-09-04 09:59:29 +0100

    small change

--- data/post_grad_thesis.js
+++ data/post_grad_thesis.js
@@ -4,8 +4,8 @@ var post_grad_thesis =
         [ 	"MSc in Software Development Thesis" ,
             "A Model Checking Tool to detect and resolve potential Redundancies in Entity Relationship models" ,
             "The aims are to prototype an enhanced version of a discovery strategy proposed by Bowers (2002) and verify how efficient and correct this solution is in discovering potential redundant relationships" ,
-            "summary|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf" +
-            "|full version|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/dissertation.pdf" ,
+            "Exec summary|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/web_poster.pdf" +
+            "|Full version|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/docs/dissertation.pdf" ,
             "Java" ,
             "/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/computing/model_checking_tool/code" ,
             "Mar-2008",
@@ -13,9 +13,9 @@ var post_grad_thesis =
         [ 	"Maitrise Banque Finance Assurance" ,
             "Credit Value-at-Risk (VaR)" ,
             "Description and implementation  of the VaR applied to a credit portfolio and review of the methodology limitations" ,
-            "table of content|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_sommaire.pdf" +
-            "|introduction|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_introduction.pdf" +
-            "|part I|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_partie1.pdf" ,
+            "Table of content|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_sommaire.pdf" +
+            "|Introduction|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_introduction.pdf" +
+            "|Part 1|/ProjectPortfolio_2.0/tree/master/projects/post_grad_thesis/finance/credit_var/docs/credit_var_limites_partie1.pdf" ,
             "" ,
             "" ,
             "Jun-1998",
