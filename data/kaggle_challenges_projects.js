1a7d0b0a09bf1ca23ad2c51776ff01a8e39652fb
freddy <freddy.marechal@gmail.com>
2017-10-11 22:52:29 +0100

    addition of a Kaggle project

---
+++ data/kaggle_challenges_projects.js
@@ -0,0 +1,12 @@
+var artificial_intelligence_projects =
+    [
+        [ 	"Supervised classification Learning" ,
+            "Santander Customer Satisifaction Prediction" ,
+            "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve a customer's happiness before it's too late " +
+            "(https://www.kaggle.com/c/santander-customer-satisfaction)" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/docs/kaggle_santander_customer_satisfaction.pdf" ,
+            "R" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/code" ,
+            "Sep-2017",
+            "EN"]
+    ];
