ad25aa14779814160114b9fae0336aeb25907346
freddy <freddy.marechal@gmail.com>
2017-10-11 23:10:48 +0100

    small change

--- data/kaggle_challenges_projects.js
+++ data/kaggle_challenges_projects.js
@@ -2,7 +2,7 @@ var kaggle_challenges_projects =
     [
         [ 	"Supervised classification Learning" ,
             "Santander Customer Satisifaction Prediction" ,
-            "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve a customer's happiness before it's too late. (https://www.kaggle.com/c/santander-customer-satisfaction)" ,
+            "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve a customer's happiness before it's too late. (https://www.kaggle.com/c/santander-customer-satisfaction)" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/docs/kaggle_santander_customer_satisfaction.pdf" ,
             "R" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/code" ,
