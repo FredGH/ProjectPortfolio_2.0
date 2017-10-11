a63b56d1447204daee5c680896f294692d038788
freddy <freddy.marechal@gmail.com>
2017-10-11 23:36:32 +0100

    bug fix

--- data/kaggle_challenges_projects.js
+++ data/kaggle_challenges_projects.js
@@ -12,7 +12,7 @@ var kaggle_challenges_projects =
             "Orange Customer Satisfaction Prediction" ,
             "Help Orange to predict the propensity of customers to switch provider (churn), buy new products or services (appetency), or buy upgrades or add-ons proposed to them to make the sale more profitable (up-selling). (http://www.kdd.org/kdd-cup/view/kdd-cup-2009)" ,
             "Presentation|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_presentation.pdf" +
-            "|Report|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_full_report.pdf" +
+            "|Report|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_full_report.pdf",
             "R" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/code" ,
             "May-2017",
