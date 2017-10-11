019733ff8de130114af9509f206f61de79d86356
freddy <freddy.marechal@gmail.com>
2017-10-11 23:30:22 +0100

    more

--- data/kaggle_challenges_projects.js
+++ data/kaggle_challenges_projects.js
@@ -1,6 +1,6 @@
 var kaggle_challenges_projects =
     [
-        [ 	"Supervised classification Learning" ,
+        [ 	"Supervised Classification Machine Learning" ,
             "Santander Customer Satisifaction Prediction" ,
             "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve a customer's happiness before it's too late. (https://www.kaggle.com/c/santander-customer-satisfaction)" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/docs/kaggle_santander_customer_satisfaction.pdf" ,
@@ -8,7 +8,7 @@ var kaggle_challenges_projects =
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/code" ,
             "Sep-2017",
             "EN"],
-        [ 	"Supervised classification Learning" ,
+        [ 	"Supervised Classification Machine Learning" ,
             "Orange Customer Satisfaction Prediction" ,
             "Help Orange to predict the propensity of customers to switch provider (churn), buy new products or services (appetency), or buy upgrades or add-ons proposed to them to make the sale more profitable (up-selling). (http://www.kdd.org/kdd-cup/view/kdd-cup-2009)" ,
             "Presentation|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_presentation.pdf" +
