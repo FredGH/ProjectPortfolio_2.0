9d0787b7d78aa5cf2570c7cbffbc8a031b16eb99
freddy <freddy.marechal@gmail.com>
2017-10-12 23:16:34 +0100

    nosql projects

---
+++ data/no_sql_projects.js
@@ -0,0 +1,19 @@
+var no_sql_projects =
+    [
+        [ 	"Programming",
+            "Flight Booking System in Cassandra" ,
+            "Detailed report explaining the design/build and implementation of a simplified flight booking system." ,
+            "/ProjectPortfolio_2.0/tree/master/projects/no_sql/cassandra_java/docs/flight_booking_system.pdf" ,
+            "CQL/Java" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/no_sql/cassandra_java/code",
+            "Sep-2016",
+            "EN"],
+        [ 	"Notes",
+            "High Level Cassandra Concepts" ,
+            "Summarise some important concepts relating to the Cassandra database" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/no_sql/cassandra_java/docs/cassandra_notes.pdf" ,
+            "" ,
+            "",
+            "Sep-2016",
+            "EN"]
+    ];
