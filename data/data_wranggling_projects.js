98cc4d6551d20968931f27e45d33e0bbc2409594
freddy <freddy.marechal@gmail.com>
2017-10-13 23:29:31 +0100

    changes

---
+++ data/data_wranggling_projects.js
@@ -0,0 +1,11 @@
+var data_wranggling_projects =
+    [
+        [ 	"Programming",
+            "Telecom Companies Report in Spark SQL" ,
+            "Detailed report explaining the design/build and implementation of a reporting solution to find the top 10 customers facing frequent call drops." ,
+            "/ProjectPortfolio_2.0/tree/master/projects/big_data_engineering/spark_sql/ex_2/docs/telecom_companies_report.pdf" ,
+            "Spark SQL / Scala" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/big_data_engineering/spark_sql/ex_2/code",
+            "Aug-2016",
+            "EN"]
+    ];
