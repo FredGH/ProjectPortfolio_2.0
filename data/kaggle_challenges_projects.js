2d924dd87bbaa78a7205f6e071b7956cf6bacfd6
freddy <freddy.marechal@gmail.com>
2017-10-13 23:34:40 +0100

    small changes

--- data/kaggle_challenges_projects.js
+++ data/kaggle_challenges_projects.js
@@ -2,7 +2,7 @@ var kaggle_challenges_projects =
     [
         [ 	"Supervised Classification Machine Learning" ,
             "Santander Customer Satisifaction Prediction" ,
-            "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve customers' happiness before it's too late. https://www.kaggle.com/c/santander-customer-satisfaction" ,
+            "Help Santander identify dissatisfied customers early in their relationship to take proactive steps to improve customers' happiness.<p></p> https://www.kaggle.com/c/santander-customer-satisfaction" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/docs/kaggle_santander_customer_satisfaction.pdf" ,
             "R" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/code" ,
@@ -10,7 +10,7 @@ var kaggle_challenges_projects =
             "EN"],
         [ 	"Supervised Classification Machine Learning" ,
             "Orange Customer Satisfaction Prediction" ,
-            "Help Orange to predict the propensity of customers to switch provider (churn)/buy new products (appetency)  or upgrade (up-selling) ." ,
+            "Help Orange to predict the propensity of customers to switch provider (churn)/buy new products (appetency)  or upgrade (up-selling). <p></p> http://www.kdd.org/kdd-cup/view/kdd-cup-2009" ,
             "Presentation|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_presentation.pdf" +
             "|Report|/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/docs/kaggle_orange_customer_relationship_prediction_full_report.pdf",
             "R" ,
