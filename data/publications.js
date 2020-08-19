a256d06299cd8bc283226a9bb55a299fa2baa8a1
freddy <freddy.marechal@gmail.com>
2020-08-19 08:10:34 +0100

    bug fix

--- data/publications.js
+++ data/publications.js
@@ -9,7 +9,8 @@ var publications =
             "May-2019",
             "EN",
             1]
-   
+		,
+  
         [ 	"Conference Publication (First Author)" ,
             "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
             "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
