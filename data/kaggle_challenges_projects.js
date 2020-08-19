88f3fe41f5cbed2d09b108ba9b0610464749205a
freddy <freddy.marechal@gmail.com>
2020-08-19 06:53:59 +0100

    addition of new documents

--- data/kaggle_challenges_projects.js
+++ data/kaggle_challenges_projects.js
@@ -8,7 +8,7 @@ var kaggle_challenges_projects =
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/santander_customer_satisfaction_competition/code" ,
             "Sep-2017",
             "EN",
-            2],
+            3],
         [ 	"Supervised Classification Machine Learning" ,
             "Orange Customer Satisfaction Prediction" ,
             "Help Orange to predict the propensity of customers to switch provider (churn)/buy new products (appetency)  or upgrade (up-selling). <p></p> http://www.kdd.org/kdd-cup/view/kdd-cup-2009" ,
@@ -17,6 +17,15 @@ var kaggle_challenges_projects =
             "R" ,
             "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/orange_customer_satisfaction_competition/code" ,
             "May-2017",
-            "FR",
-            1]
+            "EN",
+            2]/*,
+		[ 	"Supervised Classification Machine Learning" ,
+            "The Insurance Company Benchmark" ,
+            "Help a Dutch insurance company to build a caravan insurance purchase policy prediction model to target potentially interested customers, as well as discover the main drivers influencing the purchase of a caravan insurance decision.<p></p>"http://kdd.ics.uci.edu/databases/tic/tic.html" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/the_insurance_company_benchmark/docs/capstone_report_v2.pdf" +
+            "R" ,
+            "/ProjectPortfolio_2.0/tree/master/projects/kaggle_challenges/the_insurance_company_benchmark/code" ,
+            "Aug-2020",
+            "EN",
+            1]*/
     ];
---
+++ projects/post_grad_thesis/machine_learning/stock_mkt_trend_pred/code/ReadMe.txt
@@ -0,0 +1,2 @@
+The important code sections are available in the appendix of the thesis.
+Full Code available on demand. Please contact freddy.marechal@gmail.com.
