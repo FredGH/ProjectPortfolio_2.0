dcdf1bdcbe5477b96f07205ead0eee2ce706f160
freddy <freddy.marechal@gmail.com>
2020-08-19 08:17:50 +0100

    small change

--- data/publications.js
+++ data/publications.js
@@ -1,5 +1,15 @@
 var publications =
    [
+		[ 	"Conference Publication (First Author)" ,
+            "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
+            "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
+			"http://research.gold.ac.uk/26368/",
+            "" ,
+            "" ,
+            "May-2019",
+            "EN",
+            2],
+			
         [ 	"Conference Publication (First Author)" ,
             "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
             "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
@@ -8,6 +18,6 @@ var publications =
             "" ,
             "Aug-2018",
             "EN",
-            2]
+            1]
     ];
     
