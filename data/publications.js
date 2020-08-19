8c9daef8a0e94f76006560aca26ad016466b4cdc
freddy <freddy.marechal@gmail.com>
2020-08-19 07:07:24 +0100

    addition of missing files

---
+++ data/publications.js
@@ -0,0 +1,21 @@
+var publications_projects =
+    [
+        [ 	"Programming" ,
+            "The Neighborhood Map" ,
+            "Single-page application featuring a map of my neighborhood." ,
+            "/NeighborhoodMap/master/README.md" ,
+            "Knockout/Javascript/CSS" ,
+            "/NeighborhoodMap/master/index.html" ,
+            "Jun-2016",
+            "EN",
+            2],
+        [ 	"Programming" ,
+            "Frogger" ,
+            "Code player, enemies, and other game entities in JavaScript in object-oriented pseudo-classical style, given the art assets and game engines.",
+            "/Frogger/master/README.md" ,
+            "Javascript/CSS" ,
+            "/Frogger/master/index.html" ,
+            "Apr-2016",
+            "EN",
+            1]
+    ];
--- projects/post_grad_thesis/machine_learning/stock_mkt_trend_pred/code/ReadMe.txt
+++ projects/post_grad_thesis/machine_learning/stock_mkt_trend_pred/code/ReadMe.txt
@@ -1,2 +1,2 @@
 The important code sections are available in the appendix of the thesis.
-Full Code available on demand. Please contact freddy.marechal@gmail.com.
+Full Code available on demand. 
