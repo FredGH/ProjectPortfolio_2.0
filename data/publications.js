d772049876be6d8df8d92260d7dcc7a20915eca6
freddy <freddy.marechal@gmail.com>
2020-08-19 07:48:16 +0100

    additional info

--- data/publications.js
+++ data/publications.js
@@ -2,8 +2,11 @@ var publications =
    [
         [ 	"Conference Publication" ,
             "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
-            "Paper available from http://research.gold.ac.uk/24130/" ,
-            "",
+            "Collective intelligence represented as sentiment extracted "+
+			"from social media mining found applications in various areas. Numerous studies involving machine learning modelling have demonstrated that such sentiment information may or may not have predictive power on the stock market trend. This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant. The proposed volatility " +
+			"prediction modelling approach, based on Jordan and Elman recurrent neural networks, demonstrates that the addition of sentiment or sentiment moment reduces the prediction root mean square error (RMSE) to about one third. The experiments we conducted also demonstrate " +
+			"that the addition of sentiment reduces the RMSE for 24 out of the 36 stocks/constituents, representing 87.9% of the index weight. This is the first study in the literature relating to the prediction of the market trend or the volatility based on an index and its constituents’ sentiment." ,
+            "http://research.gold.ac.uk/24130/",
             "" ,
             "" ,
             "Aug-2018",
