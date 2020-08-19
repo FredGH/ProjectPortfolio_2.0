ef4786e247b877ddbc716d0fd5a353efbfae0c91
freddy <freddy.marechal@gmail.com>
2020-08-19 08:25:12 +0100

    latest version

--- data/publications.js
+++ data/publications.js
@@ -1,8 +1,8 @@
 var publications =
    [
-		[ 	"Conference Publication (First Author)" ,
-            "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
-            "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
+		[ 	"Conference Paper" ,
+            "Predicting SP500 based on its constituents and their social media derived sentiment" ,
+            "This work proposes, for the first time, an approach to predicting SP500 based on the closing stock prices and sentiment data of the SP500 constituents. One of the significant complexities of our framework is due to the high dimensionality of the dataset to analyse, which is based on a large number of constituents and their sentiments, and their lagging.",
 			"http://research.gold.ac.uk/26368/",
             "" ,
             "" ,
@@ -10,9 +10,9 @@ var publications =
             "EN",
             2],
 			
-        [ 	"Conference Publication (First Author)" ,
+        [ 	"Conference Paper" ,
             "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
-            "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
+            "This research investigates the predictive information of sentiment on the XLE index market trend and volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here, the sentiment’s impact on volatility predictions is significant.",
 			"http://research.gold.ac.uk/24130/",
             "" ,
             "" ,
