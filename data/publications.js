8e893679105d7516a0c2a288c583627a4c93fa81
freddy <freddy.marechal@gmail.com>
2020-08-19 08:13:19 +0100

    small change

--- data/publications.js
+++ data/publications.js
@@ -1,16 +1,14 @@
 var publications =
    [
-           [ 	"Conference Publication" ,
-            "Predicting S&P 500 based on its constituents and their social media derived sentiment" ,
-            " This work proposes, for the first time, an approach to predicting S&P 500 based on the closing stock prices and sentiment data of the S&P 500 constituents. One of the significant complexities of our framework is due to the high dimensionality of the dataset to analyse, which is based on a large number of constituents and their sentiments, and their lagging",
+	   [ 	"Conference Publication" ,
+			"Predicting S&P 500 based on its constituents and their social media derived sentiment" ,
+			" This work proposes, for the first time, an approach to predicting S&P 500 based on the closing stock prices and sentiment data of the S&P 500 constituents. One of the significant complexities of our framework is due to the high dimensionality of the dataset to analyse, which is based on a large number of constituents and their sentiments, and their lagging",
 			"http://research.gold.ac.uk/26368/",
-            "" ,
-            "" ,
-            "May-2019",
-            "EN",
-            1]
-		,
-  
+			"" ,
+			"" ,
+			"May-2019",
+			"EN",
+			1],
         [ 	"Conference Publication (First Author)" ,
             "On XLE index constituents’ social media based sentiment informing the index trend and volatility prediction" ,
             "This research investigates the predictive information of sentiment regarding the Energy Select Sector related XLE index and of its constituents, on the index and its volatility, based on a novel robust machine learning approach. While we demonstrate that sentiment does not have any impact on any of the trend prediction scenarios investigated here related to XLE and its constituents, the sentiment’s impact on volatility predictions is significant.",
